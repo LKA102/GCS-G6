6f46c1dc6d5c172de578a38f25c6ec38eeb731b3
JoseOrtizUNMSM <edwin.ortiz1@unmsm.edu.pe>
2022-06-30 22:39:57 -0500

    ProyectoFollow

--- Desarrollo/FC/Fuentes/src/Test/ProyectoFollow.java
+++ Desarrollo/FC/Fuentes/src/Test/ProyectoFollow.java
@@ -1,4 +1,15 @@
-package Test;
+ package Test;
 
 import JDBC.ConexionBD;
 import vista.principal.login;
+
+public class ProyectoFollow {
+    public static void main(String[] args) throws ClassNotFoundException {
+        login abrir=new login();
+        ConexionBD objetoConexion = new ConexionBD();
+        objetoConexion.conectar();
+        abrir.setVisible(true);
+        //abrir.setLocationRelativeTo(null);
+    }
+    
+}
